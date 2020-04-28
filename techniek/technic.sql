24dfbef01b0fb81374992e5599e17bdc217f6ac9
wil2005-development <56365974+wil2005-development@users.noreply.github.com>
2020-04-28 14:38:59 +0200

    Update technic.sql

--- techniek/technic.sql
+++ techniek/technic.sql
@@ -2,7 +2,7 @@ INSERT INTO `jobs` (`name`, `label`) VALUES
 ('techniek', 'Enexis');
 
 INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-('techniek', 0, 'Mederwerker', 'Medewerker', 800, '{}', '{}');
+('techniek', 0, 'Mederwerker', 'Medewerker', 50, '{}', '{}');
 
 INSERT INTO `items` (`name`, `label`,`rare`, `can_remove`) VALUES
 ('contrat', 'Bills', 15, 1);
