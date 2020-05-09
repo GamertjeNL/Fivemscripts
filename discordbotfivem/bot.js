15712d0b58fac9e8999cb47cfe1f59dd3a9556d6
wil2005-development <56365974+wil2005-development@users.noreply.github.com>
2020-05-09 15:49:53 +0200

    UPDATE

--- discordbotfivem/bot.js
+++ discordbotfivem/bot.js
@@ -2,9 +2,10 @@ const { Client } = require('discord.js');
 const client = new Client;
 const {updatePlayerCount} = require("./utils/")
 global.config = require("./config.json")
-
+// invite your bot
+// https://discordapp.com/oauth2/authorize?client_id=your_client_id&scope=bot&permissions=8
 client.on('ready', () => {
-    console.log(`Discord bot is ingelogt als: ${client.user.tag}`);
+    console.log(`Discord Bot logged in as ${client.user.tag}`);
     updatePlayerCount(client, 5)
     //this will update the bot's activity every 5 seconds
 });
