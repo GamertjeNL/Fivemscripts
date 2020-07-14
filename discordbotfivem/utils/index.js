5584766a1cfe8d9bd9e94838d0fb6b86a368b4c8
wil2005-development <56365974+wil2005-development@users.noreply.github.com>
2020-07-14 15:03:46 +0200

    Update voor discord bot

--- discordbotfivem/utils/index.js
+++ discordbotfivem/utils/index.js
@@ -2,8 +2,8 @@ module.exports = {
     /**
      * updatePlayerCount
      * Constantly updates the player count of the server at a steady rate
-     * @param {object} //client The client of the bot object
-     * @param {number} //seconds The integer amount for the derivate of # of times it refreshes the bot's activity
+     * @param {object} client The client of the bot object
+     * @param {number} seconds The integer amount for the derivate of # of times it refreshes the bot's activity
      * ```js
      * const {updatePlayerCount} = require("./utils/")
      * const { Client } = require('discord.js');
@@ -14,11 +14,9 @@ module.exports = {
      */
     updatePlayerCount: (client, seconds) => {
         const interval = setInterval(function setStatus() {
-            status = `${GetNumPlayerIndices()} player(s)`
-            //console.log(status)
+            status = `${GetPlayerIndices()} speler(s)`
             client.user.setActivity(status, {type: 'WATCHING'})
             return setStatus;
         }(), seconds * 1000)
     }
-
 }
